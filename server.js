8a7a83f327cf09b38bd6c11b66db8a0b509a351e
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2024-12-24 23:25:46 +0530

    @rohit-deka1

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+});
+
+
+app.get('/landing-page', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'landing.html')); 
+});
+
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
