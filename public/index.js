53563c0b7ec521c6e92d30daad4f1b9ffa167793
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2024-12-26 18:15:50 +0530

    responsive

--- public/index.js
+++ public/index.js
@@ -19,6 +19,12 @@ $(document).ready(function () {
         $('#form-container').fadeOut();
     })
 
+    $(document).on('keydown', function(event) {
+        if ((event.key === "Escape") || (event.keyCode === 27)) {
+            $('#form-container').fadeOut();
+        }
+    });
+
 });
 
 
